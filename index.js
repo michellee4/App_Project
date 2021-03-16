8909c0194155370c837c688a59a7c7ed9eeb421e
Michelle Guerrero <michelleguerrero@Michelles-MacBook-Pro.local>
2021-03-16 14:20:44 -0700

    adding data

--- index.js
+++ index.js
@@ -2,32 +2,33 @@
 let PlantArray = [];
 
 // define a constructor to create movie objects
-let PlantObject = function (pname, psciname, _family, pclimate, pcolor, pURL) {
+let PlantObject = function(pname, psciname, _family, pclimate, pcolor, pURL) {
     this.name = pname;
     this.sciname = psciname;
     this.ID = PlantArray.length + 1;
-    this.pfamily = _family; 
+    this.pfamily = _family;
     this.climate = pclimate;
     this.color = pcolor;
     this.URL = pURL;
 }
 
+plantArray.push(new PlantObject("Eucalyptus", "Eucalyptus Globulus", "Myrtle family", "Green", "https://www.thespruce.com/how-to-grow-and-use-eucalyptus-1762354"))
 
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
 
     createList();
 
-// add button events ************************************************************************
-    
-    document.getElementById("buttonAdd").addEventListener("click", function () {
+    // add button events ************************************************************************
+
+    document.getElementById("buttonAdd").addEventListener("click", function() {
         PlantArray.push(new PlantObject(document.getElementById("pname").value, document.getElementById("sciname").value,
-        document.getElementById("pfamily").value, document.getElementById("climate").value, document.getElementById("pcolor").value));
+            document.getElementById("pfamily").value, document.getElementById("climate").value, document.getElementById("pcolor").value));
         document.location.href = "index.html#ListAll";
         // also add the URL value
     });
-    
-    document.getElementById("buttonClear").addEventListener("click", function () {
+
+    document.getElementById("buttonClear").addEventListener("click", function() {
         document.getElementById("pname").value = "";
         document.getElementById("sciname").value = "";
         document.getElementById("pfamily").value = "";
@@ -35,41 +36,41 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("pcolor").value = "";
     });
 
-    $(document).bind("change", "#psize", function (event, ui) {
+    $(document).bind("change", "#psize", function(event, ui) {
         selectedGenre = $('#psize').val();
     });
 
-    $(document).bind("change", "#mlevel", function (event, ui) {
+    $(document).bind("change", "#mlevel", function(event, ui) {
         selectedGenre = $('#mlevel').val();
     });
 
-  
-// end of add button events ************************************************************************
 
-  
-  
-// page before show code *************************************************************************
+    // end of add button events ************************************************************************
+
+
+
     // page before show code *************************************************************************
-    $(document).on("pagebeforeshow", "#ListPlants", function (event) {   // have to use jQuery 
+    // page before show code *************************************************************************
+    $(document).on("pagebeforeshow", "#ListPlants", function(event) { // have to use jQuery 
         createList();
     });
 
- 
-// end of page before show code *************************************************************************
 
-});  
+    // end of page before show code *************************************************************************
+
+});
 // end of wait until document has loaded event  *************************************************************************
 
 function createList() {
     // clear prior data
     var divPlantList = document.getElementById("divPlantList");
-    while (divPlantList.firstChild) {    // remove any old data so don't get duplicates
+    while (divPlantList.firstChild) { // remove any old data so don't get duplicates
         divPlantList.removeChild(divPlantList.firstChild);
     };
 
     var ul = document.createElement('ul');
 
-    PlantArray.forEach(function (element,) {   // use handy array forEach method
+    PlantArray.forEach(function(element, ) { // use handy array forEach method
         var li = document.createElement('li');
         li.innerHTML = element.name + "  (" + element.pfamily + ")";
         ul.appendChild(li);
@@ -78,11 +79,6 @@ function createList() {
 
 };
 
-function AddPlant()
-{
+function AddPlant() {
     //code goes here but your tired so you'll come back at the last minute like a champion//
 }
-  
-
-
-
